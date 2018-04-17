be3a0f2796835524b608b1620561dc2ab16d6c08
libraiger <misssilverbeauty0927@outlook.com>
2018-04-18 00:46:32 +0800

    Feature * Using any http means possible

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,31 @@
-import * as nodeStatic from 'node-static'
 import * as http from 'http'
-
-const file = new nodeStatic.Server()
+import * as url from 'url'
+import * as path from 'path'
+import * as fs from 'fs'
 
 http.createServer((request, response) => {
-  request.addListener('end', () => {
-    file.serve(request, response)
-  }).resume()
-}).listen(process.env.PORT || 80)
+  const uri = url.parse(request.url).pathname
+  let filename = path.join(process.cwd(), uri)
+
+  fs.exists(filename, (exists) => {
+    if(!exists || filename.includes('server')) {
+      response.writeHead(404, {'Content-Type':'text/plain'})
+      response.write('404 Not Found\n')
+      response.end()
+    } else {
+      if(fs.statSync(filename).isDirectory()) filename += '/index.html'
+
+      fs.readFile(filename, 'binary', (error, file) => {
+        if(error) {
+          response.writeHead(500, {'Content-Type':'text/plain'})
+          response.write(error + '\n');
+          response.end()
+        } else {
+          response.writeHead(200)
+          response.write(file, 'binary')
+          response.end()
+        }
+      })
+    }
+  })
+}).listen(parseInt(process.env.PORT) || 8888)
